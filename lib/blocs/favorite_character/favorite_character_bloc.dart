ee644719e58b842e850fa7df925e874d8cb6c6cd
Nicolas Paris <nicolas.paris@volune.fr>
2022-11-03 13:47:35 +0100

    Add load favorite characters bloc event

--- lib/blocs/favorite_character/favorite_character_bloc.dart
+++ lib/blocs/favorite_character/favorite_character_bloc.dart
@@ -3,15 +3,22 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
-import '../../models/favorite_character.dart';
+import '../../dao/character_dao.dart';
+import '../../models/character.dart';
 
 part 'favorite_character_event.dart';
 part 'favorite_character_state.dart';
 
 class FavoriteCharacterBloc extends Bloc<FavoriteCharacterEvent, FavoriteCharacterState> {
-  final FavoriteCharacterDao favoriteCharacterDao;
+  final CharacterDao favoriteCharacterDao;
 
   FavoriteCharacterBloc(this.favoriteCharacterDao) : super(FavoriteCharacterInitial()) {
+
+    on<LoadFavoriteCharacters>((event, emit) async {
+      final results = await favoriteCharacterDao.findAllFavoriteCharacters();
+      emit(FavoriteCharactersLoaded(results));
+    });
+
     on<InsertNewFavoriteCharacter>((event, emit) async {
       await favoriteCharacterDao.insertFavoriteCharacter(event.favoriteCharacter);
     });
--- lib/blocs/favorite_character/favorite_character_event.dart
+++ lib/blocs/favorite_character/favorite_character_event.dart
@@ -3,8 +3,10 @@ part of 'favorite_character_bloc.dart';
 @immutable
 abstract class FavoriteCharacterEvent {}
 
+class LoadFavoriteCharacters extends FavoriteCharacterEvent {}
+
 class InsertNewFavoriteCharacter extends FavoriteCharacterEvent {
-  final FavoriteCharacter favoriteCharacter;
+  final Character favoriteCharacter;
 
   InsertNewFavoriteCharacter(this.favoriteCharacter);
 }
