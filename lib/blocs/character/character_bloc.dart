d58c34bac037a12769827e0c579652566445e509
Nicolas Paris <nicolas.paris@volune.fr>
2022-11-03 16:58:32 +0100

    Add characterDao in CharacterBloc

--- lib/blocs/character/character_bloc.dart
+++ lib/blocs/character/character_bloc.dart
@@ -4,16 +4,18 @@ import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:rick_and_morty_characters_app/services/rick_and_morty_service.dart';
 
+import '../../dao/character_dao.dart';
 import '../../models/character.dart';
 
 part 'character_event.dart';
 part 'character_state.dart';
 
 class CharacterBloc extends Bloc<CharacterEvent, CharacterState> {
+  final CharacterDao favoriteCharacterDao;
   List<Character> characters = [];
   RickAndMortyService rickAndMortyService = RickAndMortyService();
 
-  CharacterBloc() : super(CharacterInitial()) {
+  CharacterBloc(this.favoriteCharacterDao) : super(CharacterInitial()) {
 
     on<LoadCharacters>((event, emit) async {
       characters = await rickAndMortyService.getAllCharacters();
