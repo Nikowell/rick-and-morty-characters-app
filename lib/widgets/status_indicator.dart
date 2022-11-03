9c37a02f9901e658618dde8ab2ba48c3851d28df
Nicolas Paris <nicolas.paris@volune.fr>
2022-11-03 10:46:34 +0100

    Add status circle indicator widget

---
+++ lib/widgets/status_indicator.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class StatusIndicator extends StatelessWidget {
+  final String characterStatus;
+
+  const StatusIndicator({Key? key, required this.characterStatus}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 10.0,
+      height: 10.0,
+      decoration: BoxDecoration(
+        color: _getColorByStatus(characterStatus),
+        shape: BoxShape.circle,
+      ),
+    );
+  }
+
+  Color _getColorByStatus(String status) {
+    var enumStatus = Status.values.singleWhere((element) => element.name == status);
+
+    switch (enumStatus) {
+      case Status.Alive:
+        return Colors.lightGreen;
+      case Status.Dead:
+        return Colors.red;
+      default:
+        return Colors.grey;
+    }
+  }
+}
+
+enum Status {
+  Alive,
+  Dead,
+  unknown
+}
