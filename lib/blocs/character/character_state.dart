36aa051a57cf859d118e6a3f811fd029a5b7f558
Nicolas Paris <nicolas.paris@volune.fr>
2022-11-04 09:06:00 +0100

    Add CharacterLoading state

--- lib/blocs/character/character_state.dart
+++ lib/blocs/character/character_state.dart
@@ -5,6 +5,8 @@ abstract class CharacterState {}
 
 class CharacterInitial extends CharacterState {}
 
+class CharacterLoading extends CharacterState {}
+
 class CharacterLoaded extends CharacterState {
   final List<Character> characters;
 
