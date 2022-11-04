2ebc80f57fd9f4941c1335e61b217a843875d001
Nicolas Paris <nicolas.paris@volune.fr>
2022-11-04 09:07:05 +0100

    Add LoadMoreCharacters event mapping

--- lib/blocs/character/character_bloc.dart
+++ lib/blocs/character/character_bloc.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:collection/collection.dart';
 import 'package:meta/meta.dart';
+import 'package:rick_and_morty_characters_app/models/api_schema.dart';
 import 'package:rick_and_morty_characters_app/services/rick_and_morty_service.dart';
 
 import '../../dao/character_dao.dart';
@@ -13,6 +15,7 @@ part 'character_state.dart';
 class CharacterBloc extends Bloc<CharacterEvent, CharacterState> {
   final CharacterDao favoriteCharacterDao;
   List<Character> characters = [];
+  late ApiSchema apiSchema;
   RickAndMortyService rickAndMortyService = RickAndMortyService();
 
   CharacterBloc(this.favoriteCharacterDao) : super(CharacterInitial()) {
@@ -26,6 +29,21 @@ class CharacterBloc extends Bloc<CharacterEvent, CharacterState> {
       }
 
       emit(CharacterLoaded(characters));
+    });
+
+    on<LoadMoreCharacters>((event, emit) async {
+      emit(CharacterLoading());
+      apiSchema = await rickAndMortyService.getCharacters(apiSchema.info.next!);
+      characters += apiSchema.results;
+      List<Character> favoriteCharacters = await favoriteCharacterDao.findAllFavoriteCharacters();
+
+      for (var favoriteCharacter in favoriteCharacters) {
+        Character? character = characters.firstWhereOrNull((element) => element.id == favoriteCharacter.id);
+        if (character != null) {
+          character.isFavorite = true;
+        }
+      }
+      emit(CharacterLoaded(apiSchema.results));
     });
 
     on<UpdateFavoriteStatus>((event, emit) async {
